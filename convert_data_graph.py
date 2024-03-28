534c112d3d8a46ef1db2d5a13e0d68afa1215eda
AQUINO SANCHEZ Jorge <jorge.aquinosanchez@ext.europarl.europa.eu>
2024-03-28 17:04:01 +0100

    update graph add madsrdf:ComplexSubject  node

--- convert_data_graph.py
+++ convert_data_graph.py
@@ -1,14 +1,18 @@
 import os
 import pandas as pd
 from rdflib import Graph, URIRef, Namespace, Literal
+from rdflib.collection import Collection
+from rdflib.term import BNode, Node
 
-# Declaration Global
+# Declaration Global NAMESPACE
 ns_jurivoc = Namespace("https://fedlex.data.admin.ch/vocabulary/jurivoc/")
 ns_thesaurus = Namespace("https://fedlex.data.admin.ch/vocabulary/jurivoc#")
 ns_rdf = Namespace("http://www.w3.org/1999/02/22-rdf-syntax-ns#")
+ns_rdfs = Namespace("http://www.w3.org/2000/01/rdf-schema#")
 ns_skos = Namespace("http://www.w3.org/2004/02/skos/core#")
 ns_dct = Namespace("http://purl.org/dc/terms/")
 ns_owl = Namespace("http://www.w3.org/2002/07/owl#")
+ns_madsrdf = Namespace("http://www.loc.gov/mads/rdf/v1#")
 
 class update_graph:
 
@@ -133,9 +137,6 @@ class update_graph:
     
     #def find_uri_graph(self,subject,altlabel_value:list,preflabel_value:list) -> str:
 
-
-
-
     def update_uri_concepts(self):
 
         if len(self.graphCurrent) > 0:
@@ -161,82 +162,113 @@ class convert_graph:
 
         self.jurivocGraph.bind("jurivoc",ns_jurivoc)
         self.jurivocGraph.bind("thesaurus",ns_thesaurus)
+        self.jurivocGraph.bind("madsrdf",ns_madsrdf)
 
-    def generate_skos_concept(self, df:pd.DataFrame, nameGraph : str) -> Graph:
+    def generate_skos_concept(self, df:pd.DataFrame, titlekey:list) -> Graph:
         # Create a Graph
         gConcepts = Graph()
-        # Get all title header
-        titles = df["title"].to_list()
-        titlesKey = pd.Series(list(dict.fromkeys(titles)))
-        for Title in titlesKey:
-            if Title != "THÉSAURUS":
-                # get block for each title
-                dfFilter = df[df["title"].isin([Title])]
-                # find if block contain USE
-                block  = dfFilter["block"].to_list()            
-                # If bExist is false then generate a skos:Concept
-                if 'USE' not in block:
-                    # Convert title to URI            
-                    title_dq = self.dq_text(Title)
-                    title_uri = URIRef(ns_jurivoc + title_dq)
+        for Title in titlekey:
+            # get block for each title
+            dfFilter = df[df["title"].isin([Title])]
+            # find if block contain USE
+            block  = dfFilter["block"].to_list()            
+            # If bExist is false then generate a skos:Concept
+            if 'USE' not in block:
+                # Convert title to URI            
+                title_dq = self.dataquality_text(Title)
+                title_uri = URIRef(ns_jurivoc + title_dq)
+                
+                # Create skos:Concept Graph
+                gConcepts.add((title_uri,ns_rdf.type,ns_skos.Concept))
+                gConcepts.add((title_uri,ns_skos.inScheme,URIRef('https://fedlex.data.admin.ch/vocabulary/jurivoc')))
+                gConcepts.add((title_uri,ns_skos.prefLabel,Literal(Title, lang="fr")))
+                gConcepts.add((title_uri,ns_dct.identifier,Literal(title_dq)))
+                
+                for index, row in dfFilter.iterrows(): 
+                    block = row["block"]
+                    title_block = row["title_block"]
+
+                    if (len(block) > 0) and (len(title_block) > 0):
+                        # Generate Skos:altLabel
+                        if block == "UF":
+                            gConcepts.add((title_uri,ns_skos.altLabel,Literal(title_block, lang="fr")))
                     
-                    # Create skos:Concept Graph
-                    gConcepts.add((title_uri,ns_rdf.type,ns_skos.Concept))
-                    gConcepts.add((title_uri,ns_skos.inScheme,URIRef('https://fedlex.data.admin.ch/vocabulary/jurivoc')))
-                    gConcepts.add((title_uri,ns_skos.prefLabel,Literal(Title, lang="fr")))
-                    gConcepts.add((title_uri,ns_dct.identifier,Literal(title_dq)))
-                    
-                    for index, row in dfFilter.iterrows(): 
-                        block = row["block"]
-                        title_block = row["title_block"]
+                        # Generate skos:broader and skos:narrower
+                        if block == "BT":
+                            if "THÉSAURUS" in title_block:
+                                gConcepts.add((title_uri,ns_skos.topConceptOf,URIRef(ns_thesaurus + self.dataquality_text("THÉSAURUS"))))
+                                gConcepts.add((URIRef(ns_thesaurus + self.dataquality_text("THÉSAURUS")),ns_skos.hasTopConcept,title_uri))
 
-                        if (len(block) > 0) and (len(title_block) > 0):
-                            # Generate Skos:altLabel
-                            if block == "UF":
-                                gConcepts.add((title_uri,ns_skos.altLabel,Literal(title_block, lang="fr")))
-                        
-                            # Generate skos:broader and skos:narrower
-                            if block == "BT":
-                                if "THÉSAURUS" in title_block:
-                                    gConcepts.add((title_uri,ns_skos.topConceptOf,URIRef(ns_thesaurus + self.dq_text("THÉSAURUS"))))
-                                    gConcepts.add((URIRef(ns_thesaurus + self.dq_text("THÉSAURUS")),ns_skos.hasTopConcept,title_uri))
+                            else:
+                                gConcepts.add((title_uri,ns_skos.broader,URIRef(ns_jurivoc + self.dataquality_text(title_block))))
+                                # Inverse to skos:broader
+                                gConcepts.add((URIRef(ns_jurivoc + self.dataquality_text(title_block)),ns_skos.narrower,title_uri))
 
-                                else:
-                                    gConcepts.add((title_uri,ns_skos.broader,URIRef(ns_jurivoc + self.dq_text(title_block))))
-                                    # Inverse to skos:broader
-                                    gConcepts.add((URIRef(ns_jurivoc + self.dq_text(title_block)),ns_skos.narrower,title_uri))
+                        # Generate skos:scopeNote
+                        if block == "SN":
+                            gConcepts.add((title_uri,ns_skos.scopeNote,Literal(title_block, lang="fr") ))
 
-                            # Generate skos:scopeNote
-                            if block == "SN":
-                                gConcepts.add((title_uri,ns_skos.scopeNote,Literal(title_block, lang="fr") ))
-
-                            if block == "SA":
-                                gConcepts.add((title_uri,ns_skos.related,URIRef(ns_jurivoc + self.dq_text(title_block))))
-        
+                        if block == "SA":
+                            gConcepts.add((title_uri,ns_skos.related,URIRef(ns_jurivoc + self.dataquality_text(title_block))))
+    
         self.jurivocGraph += gConcepts
         return True
     
-    def generate_Thesaurus(self,df:pd.DataFrame, nameGraph : str) -> Graph:
+    def generate_Thesaurus(self,df:pd.DataFrame) -> Graph:
 
         # get block for each title
         gConceptScheme = Graph()
-        dfFilter = df[df["title"].isin(["THÉSAURUS"])]
-        if len(dfFilter) > 0:
-            # Convert title to URI        
-            title_uri = URIRef(ns_thesaurus + "THESAURUS")
-            # Create skos:Concept Graph
-            gConceptScheme.add((title_uri,ns_rdf.type,ns_skos.ConceptScheme))
-            gConceptScheme.add((title_uri,ns_skos.prefLabel,Literal("THÉSAURUS", lang="fr")))
-
-            for index,row in dfFilter.iterrows():
-                block = row["block"]
-                title_block = row["title_block"]
-
-                # Generate skos:scopeNote
-                if block == "SN":
-                    gConceptScheme.add((title_uri,ns_owl.versionInfo,Literal(title_block) ))        
+        # Convert title to URI        
+        title_uri = URIRef(ns_thesaurus + "THESAURUS")
+        # Create skos:Concept Graph
+        gConceptScheme.add((title_uri,ns_rdf.type,ns_skos.ConceptScheme))
+        gConceptScheme.add((title_uri,ns_skos.prefLabel,Literal("THÉSAURUS", lang="fr")))
+        for index,row in df.iterrows():
+            block = row["block"]
+            title_block = row["title_block"]
+            # Generate skos:scopeNote
+            if block == "SN":
+                gConceptScheme.add((title_uri,ns_owl.versionInfo,Literal(title_block) ))
         self.jurivocGraph += gConceptScheme
         return True    
+
+    def generate_madsrdf(self, df:pd.DataFrame, titlekey:list) -> Graph:
+
+        gSpecific = Graph()
+        log_df = []
+        for title in titlekey:
+            # Get block
+            dfTitle = df[df["title"].isin([title])]
+
+            blocks = pd.Series(dfTitle["title"].to_list()).drop_duplicates().to_list()
+            if "USE" not in blocks:
+                title_dq = self.dataquality_text(title)
+                title_uri = URIRef(ns_jurivoc+title_dq)
+
+                gSpecific.add((title_uri,ns_rdf.type,URIRef(ns_madsrdf.ComplexSubject)))
+                gSpecific.add((title_uri,ns_madsrdf.authoritativeLabel,Literal(title,lang="fr")))
+
+                componentList = []
+                for idx, row in dfTitle.iterrows():
+                    if len(row["block"]) > 0:
+                        titleBlock = self.dataquality_text(row["title_block"])
+                        uriComponent = URIRef(ns_jurivoc+titleBlock)
+                        # Collection
+                        listNode = object=uriComponent
+                        componentList.append(listNode)
+                        #
+                        gSpecific.add((uriComponent,ns_rdfs.seeAlso,title_uri))
+
+                if len(componentList) > 0:
+                    nodeList = BNode()
+                    gSpecific.add((title_uri,ns_madsrdf.componentList,nodeList))
+                    Collection(gSpecific,nodeList,componentList)                
+            else:
+                print("Warning: the {} title contain a block USE".format(title))
+                log_df.append([dfTitle])
+            
+        self.jurivocGraph += gSpecific
+        return True
 
     def generate_language_graph(self, df:pd.DataFrame) -> Graph:
 
@@ -245,7 +277,7 @@ class convert_graph:
         gLanguage = Graph()
         for index,row in df.iterrows():
             title = row["title"]
-            title_uri = URIRef(ns_jurivoc + self.dq_text(title))
+            title_uri = URIRef(ns_jurivoc + self.dataquality_text(title))
             idLang = row["language"]
             title_language = row["title_traduction"]
 
@@ -281,7 +313,7 @@ class convert_graph:
             bFlag = 'USE' in block
             if bFlag == False:
                 
-                title_uri = URIRef(ns_jurivoc + self.dq_text(Title))
+                title_uri = URIRef(ns_jurivoc + self.dataquality_text(Title))
                 title_lang = dfFilter["title"].unique()[0]
                 idLang = dfFilter["language"].unique()[0]
 
@@ -299,7 +331,7 @@ class convert_graph:
         self.jurivocGraph += gLanguage
         return True
         
-    def dq_text(self, title:str):
+    def dataquality_text(self, title:str):
 
         data_input = title.upper()
         replace_dict = {"(":"_",")":"_","[":"_","]": "_","'":"_","\"":"_"," ":"_","-":"_","Ï":"_","¿":"_","½":"_","É":"E","È":"E","Ê":"E","À":"A","Ô":"O",".":"_",",":"_","Û":"U","Î":"I","Ç":"C"}
@@ -334,6 +366,20 @@ class convert_graph:
             self.normalize_text_url(split_title,ind)
         return split_title
 
+    def get_specific_usa_and(self,df:pd.DataFrame, titlesKey:list) -> pd.DataFrame:
+        list_tmp = []
+        for title in titlesKey:
+            # Filter df
+            dfFilter = df[df["title"].isin([title])]
+            #
+            blocks = pd.Series(dfFilter["block"].to_list()).drop_duplicates().to_list()
+            if set(['USA','AND']).issubset(blocks):
+                list_tmp.append(dfFilter)
+        
+        dfTmp = pd.concat(list_tmp,ignore_index=True)
+        dfTmp.reset_index()
+        return dfTmp
+
     def graph_process(self)-> Graph:
 
         if not os.path.exists(self.output):
@@ -349,13 +395,29 @@ class convert_graph:
                 self.generate_language_graph(df)
             else:                
                 if '_fre.txt' in nameFile:
+
+                    titlesKey = pd.Series(df["title"].to_list()).drop_duplicates().to_list()
+                    
+                    # USA and AND block specific Data
+                    dfSpecific = self.get_specific_usa_and(df,titlesKey)
+                    dfSpecific.to_csv("data_usa_and.csv",sep="|",index=False)
+
                     print("Generate - Graph of data: {} # rows {}".format(nameFile, len(df)))
                     # =================== Graph THÉSAURUS
                     print("Graph THÉSAURUS")
-                    self.generate_Thesaurus(df,nameFile) 
+                    dfTHESAURUS = df[df["title"] == "THÉSAURUS"]
+                    self.generate_Thesaurus(dfTHESAURUS) 
                     # =================== Graph Skos:Concept
                     print("Graph concepts")
-                    self.generate_skos_concept(df,nameFile)                    
+                    dfConcepts = df[~df.isin(dfSpecific)].dropna()
+                    dfConcept = dfConcepts[dfConcepts["title"] != "THÉSAURUS"]
+                    self.generate_skos_concept(dfConcept,
+                                               pd.Series(dfConcept["title"].to_list()).drop_duplicates().to_list()
+                                               )
+                    # =================== Graph Specific blocks
+                    self.generate_madsrdf(dfSpecific,
+                                              pd.Series(dfSpecific["title"].to_list()).drop_duplicates().to_list()
+                                              )        
                 
                 if ('_ger.txt' in nameFile) or ('_ita.txt' in nameFile):
                     print("Generate - Graph of data: {}".format(nameFile))
