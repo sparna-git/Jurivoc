ffa990ace81af0c9e482b89e97f57aa3cd7c8fce
AQUINO SANCHEZ Jorge <jorge.aquinosanchez@ext.europarl.europa.eu>
2024-11-06 10:33:34 +0100

    update jurivos init

--- convert_data_jurivoc.py
+++ convert_data_jurivoc.py
@@ -17,14 +17,13 @@ if __name__ == '__main__':
     parser.add_argument('--d','--data',help='Path to a input file', required=True,type=pathlib.Path,dest='data')
     parser.add_argument('--o','--output',help='output Graph file directory', required=True,dest='output')
     parser.add_argument('--l','--log',help='Generate output file for each input file',dest='logs')
-    parser.add_argument('--n','--noComplexSubjects',help="Generate ComplexSubject",action=argparse.BooleanOptionalAction)
     parser.add_argument('--g','--previousVersion',help='Path to a Graph file ',type=pathlib.Path,dest='previousVersion')
-    
+    parser.add_argument('--n','--noComplexSubjects',help="No Generate ComplexSubject",required=False,action='store_true',dest='noComplexSubjects')
 
 
 	# Parse args
     args = parser.parse_args()
-    print(args)
+    
 
     print("Directory Source: {}".format(args.data))
     print("Directory output: {}".format(args.output))
@@ -70,7 +69,7 @@ if __name__ == '__main__':
     
     print("Step 2. Generate Jurivoc SKOS graph...")    
     #Instance
-    g = convert_graph(ds,args.logs, args.n)
+    g = convert_graph(ds,args.logs, args.noComplexSubjects)
     # Call process
     gOutput = g.graph_process()
     if len(gOutput) > 0:
@@ -83,7 +82,7 @@ if __name__ == '__main__':
         s = args.previousVersion
     else:
         s = ''
-    updateURIs_Concepts = update_graph(gOutput,s,args.logs,args.n)
+    updateURIs_Concepts = update_graph(gOutput,s,args.logs,args.noComplexSubjects)
     gOutputResult = updateURIs_Concepts.update_uri_concepts()
     if len(gOutputResult) > 0:
         result = os.path.join(args.output,'jurivoc.n3')
